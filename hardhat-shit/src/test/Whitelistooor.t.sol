80930f7b468669e091fcab87d774927fcde01e16
Genghis Goose <86017946+0xGenghisGoose@users.noreply.github.com>
2022-03-16 16:14:20 -0500

    first 3 tests v1 written

--- hardhat-shit/src/test/Whitelistooor.t.sol
+++ hardhat-shit/src/test/Whitelistooor.t.sol
@@ -2,11 +2,43 @@
 pragma solidity 0.8.13;
 
 import "ds-test/test.sol";
+import "src/contracts/Whitelistooor.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract WhitelistooorTest is DSTest {
-    function setUp() public {}
+contract WhitelistooorTest is Ownable, DSTest {
+    uint8 public whitelistedAddressMax;
+    uint8 public numCurrentlyWhitelisted;
+    address[] public allWhitelisted;
+    mapping(address => bool) public whitelisted;
+    Whitelistooor whitelistContract;
 
-    function testExample() public {
-        assertTrue(true);
+    constructor() {
+        msg.sender = owner();
+    }
+
+    function setUp() public {
+        // Initialize whitelist with 5 max spots
+        whitelistContract = new Whitelistooor(5);
+    }
+
+    function testNumCurrentlyWhitelisted(address addy) public {
+        assertEq(numCurrentlyWhitelisted, 0);
+        whitelistContract.addToWhitelist(addy);
+        assertEq(numCurrentlyWhitelisted, 1);
+        whitelistContract.addToWhitelist(addy);
+        assertEq(numCurrentlyWhitelisted, 2);
+    }
+
+    function testWhitelistedAddressMax() public {
+        assertEq(whitelistContract.whitelistedAddressMax(), 5);
+    }
+
+    function testFailWhitelistedAddressMax(address addy) public {
+        whitelistContract.addToWhitelist(addy); // 1st
+        whitelistContract.addToWhitelist(addy); // 2nd
+        whitelistContract.addToWhitelist(addy); // 3rd
+        whitelistContract.addToWhitelist(addy); // 4th
+        whitelistContract.addToWhitelist(addy); // 5th
+        whitelistContract.addToWhitelist(addy); // 6th address added, should revert
     }
 }
