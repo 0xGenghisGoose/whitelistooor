9e4c87dd9d17302b15d50b27c218241fbab15b8b
Genghis Goose <86017946+0xGenghisGoose@users.noreply.github.com>
2022-03-17 16:41:21 -0500

    tests complete, gas snapshot complete

--- hardhat-shit/src/test/Whitelistooor.t.sol
+++ hardhat-shit/src/test/Whitelistooor.t.sol
@@ -3,7 +3,12 @@ pragma solidity ^0.8.0;
 
 import "ds-test/test.sol";
 import "../contracts/Whitelistooor.sol";
-import "./CheatCodes.sol";
+
+interface Hevm {
+    function prank(address) external;
+
+    function expectRevert(bytes calldata) external;
+}
 
 contract WhitelistooorTest is DSTest {
     address public _owner;
@@ -13,8 +18,8 @@ contract WhitelistooorTest is DSTest {
 
     mapping(address => bool) public whitelisted;
 
-    Whitelistooor public whitelistContract;
-    CheatCodes cheats = CheatCodes(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
+    Whitelistooor whitelistContract;
+    Hevm hevm = Hevm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
 
     modifier onlyOwner() {
         require(_owner == msg.sender, "You are not the owner");
@@ -40,5 +45,21 @@ contract WhitelistooorTest is DSTest {
 
     function testWhitelistedAddressMax() public {
         assertEq(whitelistContract.whitelistedAddressMax(), 5);
+    }
+
+    function testAddToWhitelist() public onlyOwner {
+        assertEq(whitelistContract.numCurrentlyWhitelisted(), 0);
+        whitelistContract.addToWhitelist(address(1));
+        assertEq(whitelistContract.numCurrentlyWhitelisted(), 1);
+        whitelistContract.addToWhitelist(address(2));
+        assertEq(whitelistContract.numCurrentlyWhitelisted(), 2);
+        whitelistContract.addToWhitelist(address(3));
+        assertEq(whitelistContract.numCurrentlyWhitelisted(), 3);
+        whitelistContract.addToWhitelist(address(4));
+        assertEq(whitelistContract.numCurrentlyWhitelisted(), 4);
+        whitelistContract.addToWhitelist(address(5));
+        assertEq(whitelistContract.numCurrentlyWhitelisted(), 5);
+        hevm.expectRevert("Whitelist is full");
+        whitelistContract.addToWhitelist(address(6));
     }
 }
