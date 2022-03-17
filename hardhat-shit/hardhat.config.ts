2bf52cb40e5318c7206c41772f1be1a4bc6bde76
Genghis Goose <86017946+0xGenghisGoose@users.noreply.github.com>
2022-03-17 16:17:39 -0500

    debugging HH integration

--- hardhat-shit/hardhat.config.ts
+++ hardhat-shit/hardhat.config.ts
@@ -1,4 +1,5 @@
 import * as dotenv from "dotenv";
+import "tsconfig-paths/register";
 
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomiclabs/hardhat-etherscan";
@@ -13,19 +14,15 @@ const config: HardhatUserConfig = {
   solidity: "0.8.4",
   networks: {
     rinkeby: {
-      url: process.env.ALCHEMY_RINKEBY_URL || "",
+      url: process.env.ALCHEMY_RINKEBY_URL,
       accounts:
         process.env.RINKEBY_PRIVATE_KEY !== undefined
           ? [process.env.RINKEBY_PRIVATE_KEY]
           : [],
     },
   },
-  gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
-    currency: "USD",
-  },
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
+  paths: {
+    artifacts: "./out",
   },
 };
 
